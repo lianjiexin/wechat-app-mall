b1eb72ff4e47e64a5c2f4c7183e624f3c125f5ca
wangjintie <wxskkl@126.com>
2020-10-17 15:24:15 +0800

    fix(login)：自动登录

--- app.js
+++ app.js
@@ -95,22 +95,22 @@ App({
     //   }
     // }
     // 自动登录
-    // const isLogined = await AUTH.checkHasLogined();
-    // if (!isLogined) {
-    //   AUTH.login()
-    // } else {
-    //   AUTH.getUserInfo().then((res) => {
-    //     const { userInfo } = res
-    //     // 更新用户信息
-    //     WXAPI.modifyUserInfo({
-    //       avatarUrl: userInfo.avatarUrl,
-    //       city: userInfo.city,
-    //       nick: userInfo.nickName,
-    //       province: userInfo.province,
-    //       token: wx.getStorageSync('token')
-    //     })
-    //   })
-    // }
+    const isLogined = await AUTH.checkHasLogined();
+    if (!isLogined) {
+      AUTH.login()
+    } else {
+      AUTH.getUserInfo().then((res) => {
+        const { userInfo } = res
+        // 更新用户信息
+        WXAPI.modifyUserInfo({
+          avatarUrl: userInfo.avatarUrl,
+          city: userInfo.city,
+          nick: userInfo.nickName,
+          province: userInfo.province,
+          token: wx.getStorageSync('token')
+        })
+      })
+    }
   },
   globalData: {
     isConnected: true
